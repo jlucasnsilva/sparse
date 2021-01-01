10016bec26ada382b5b2eaefb0332bd5359c6192
João Lucas <jlucas.nsilva@gmail.com>
2021-01-01 17:57:31 -0300

    Changed the api of some combinators

--- parser_combinator.go
+++ parser_combinator.go
@@ -26,69 +26,77 @@ func Switch(switcher Switcher) ParserFunc {
 }
 
 // And ...
-func And(b NodeBuilder, parsers ...ParserFunc) ParserFunc {
-	return func(s Scanner) (Scanner, Node, error) {
-		var (
-			node Node
-			err  error
-			r    = s
-		)
-		for i, p := range parsers {
-			r, node, err = p(r)
-			if err != nil {
-				b.Reset()
-				return s, nil, fmt.Errorf("invalid parser at %v: %v", i, err)
+func And(parsers ...ParserFunc) func(NodeBuilder) ParserFunc {
+	return func(b NodeBuilder) ParserFunc {
+		return func(s Scanner) (Scanner, Node, error) {
+			var (
+				node Node
+				err  error
+				r    = s
+			)
+			for i, p := range parsers {
+				r, node, err = p(r)
+				if err != nil {
+					b.Reset()
+					return s, nil, fmt.Errorf("invalid parser at %v: %v", i, err)
+				}
+				b.Add(node)
 			}
-			b.Add(node)
+			return r, b.Build(), nil
 		}
-		return r, b.Build(), nil
 	}
+
 }
 
 // Some ...
-func Some(b NodeBuilder, target ParserFunc, separator ParserFunc) ParserFunc {
-	return func(s Scanner) (Scanner, Node, error) {
-		var (
-			node Node
-			err  error
-			r    = s
-			t    = r
-		)
+func Some(target ParserFunc, separator ParserFunc) func(NodeBuilder) ParserFunc {
+	return func(b NodeBuilder) ParserFunc {
+		return func(s Scanner) (Scanner, Node, error) {
+			var (
+				node Node
+				err  error
+				r    = s
+				t    = r
+			)
 
-		if r, node, err = target(r); err != nil {
-			return s, nil, errors.New("not a single match")
-		}
-		b.Add(node)
-
-		for true {
-			if t, node, err = target(r); err != nil {
-				return r, b.Build(), nil
-			}
-			if t, node, err = target(t); err != nil {
-				return r, nil, errors.New("expression ended on a separator")
+			if r, node, err = target(r); err != nil {
+				return s, nil, errors.New("not a single match")
 			}
 			b.Add(node)
-			r = t
+
+			for true {
+				if t, node, err = target(r); err != nil {
+					return r, b.Build(), nil
+				}
+				if t, node, err = target(t); err != nil {
+					return r, nil, errors.New("expression ended on a separator")
+				}
+				b.Add(node)
+				r = t
+			}
+			return s, nil, nil
 		}
-		return s, nil, nil
 	}
+
 }
 
 // Concat ...
-func Concat(b NodeBuilder, parsers ...ParserFunc) ParserFunc {
-	return func(s Scanner) (Scanner, Node, error) {
-		var (
-			node Node
-			err  error
-			r    = s
-		)
-		for _, p := range parsers {
-			r, node, err = p(r)
-			if err != nil {
-				return r, b.Build(), err
+func Concat(parsers ...ParserFunc) func(NodeBuilder) ParserFunc {
+	return func(b NodeBuilder) ParserFunc {
+		return func(s Scanner) (Scanner, Node, error) {
+			var (
+				node Node
+				err  error
+				r    = s
+			)
+			for _, p := range parsers {
+				r, node, err = p(r)
+				if err != nil {
+					return r, b.Build(), err
+				}
+				b.Add(node)
 			}
-			b.Add(node)
+			return r, b.Build(), nil
 		}
-		return r, b.Build(), nil
 	}
 }
